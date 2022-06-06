4e53996e6dc5592d5883e583dffdff8a5b861a53
Anderson L. Sergio <anderson.sergio@picpay.com>
2022-06-06 07:25:51 -0300

    Add greeting function

---
+++ hello_test.go
@@ -0,0 +1,24 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	assertCorrectMessage := func(t testing.TB, want, got interface{}) {
+		t.Helper()
+		if want != got {
+			t.Errorf("want %q got %q", want, got)
+		}
+	}
+
+	t.Run("greeting people", func(t *testing.T) {
+		want := "Hello, Chris"
+		got := Hello("Chris")
+		assertCorrectMessage(t, want, got)
+	})
+
+	t.Run("greeting default", func(t *testing.T) {
+		want := "Hello, World"
+		got := Hello("")
+		assertCorrectMessage(t, want, got)
+	})
+}
