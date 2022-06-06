1dfbf37588516057e6f0f23c0819891bd4061143
Anderson L. Sergio <anderson.sergio@picpay.com>
2022-06-06 08:29:11 -0300

    Add Spanish greeting

--- hello_test.go
+++ hello_test.go
@@ -12,13 +12,19 @@ func TestHello(t *testing.T) {
 
 	t.Run("greeting people", func(t *testing.T) {
 		want := "Hello, Chris"
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		assertCorrectMessage(t, want, got)
 	})
 
 	t.Run("greeting default", func(t *testing.T) {
 		want := "Hello, World"
-		got := Hello("")
+		got := Hello("", "")
+		assertCorrectMessage(t, want, got)
+	})
+
+	t.Run("greeting in Spanish", func(t *testing.T) {
+		want := "Hola, Elodie"
+		got := Hello("Elodie", "Spanish")
 		assertCorrectMessage(t, want, got)
 	})
 }
