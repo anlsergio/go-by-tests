9274363fb1ae9fed9c1d8d8de1065b0082b227a3
Anderson L. Sergio <anderson.sergio@picpay.com>
2022-06-07 08:41:41 -0300

    Delete redundant test

--- integers/integers_test.go
+++ integers/integers_test.go
@@ -21,17 +21,6 @@ func ExampleAdd() {
 }
 
 func TestCollectionAdd(t *testing.T) {
-	t.Run("collection of 5 numbers", func(t *testing.T) {
-		numbers := []int{1, 2, 3, 4, 5}
-
-		want := 15
-		got := CollectionAdd(numbers)
-
-		if want != got {
-			t.Errorf("want '%d', got '%d', given %v", want, got, numbers)
-		}
-	})
-
 	t.Run("collection of any size", func(t *testing.T) {
 		numbers := []int{1, 2, 3, 4, 5, 6}
 
