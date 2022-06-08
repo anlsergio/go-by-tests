f833485b2f12323ced93863efc4d353c638d9222
Anderson L. Sergio <anderson.sergio@picpay.com>
2022-06-08 07:46:50 -0300

    Add Circle calculation

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -16,14 +16,28 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{
-		Width:  12.0,
-		Height: 6.0,
-	}
-	want := 72.0
-	got := Area(rectangle)
+	t.Run("rectangle calculation", func(t *testing.T) {
+		rectangle := Rectangle{
+			Width:  12.0,
+			Height: 6.0,
+		}
+		want := 72.0
+		got := rectangle.Area()
 
-	if want != got {
-		t.Errorf("want %.2f got %.2f", want, got)
-	}
+		if want != got {
+			t.Errorf("want %.2f got %.2f", want, got)
+		}
+	})
+
+	t.Run("circle calculation", func(t *testing.T) {
+		circle := Circle{
+			Radius: 10,
+		}
+		want := 314.1592653589793
+		got := circle.Area()
+
+		if want != got {
+			t.Errorf("want %g got %g", want, got)
+		}
+	})
 }
