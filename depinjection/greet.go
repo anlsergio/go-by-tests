ae547978e614fc2effc6ecd2174b8c9fa53ce9ed
Anderson L. Sergio <anderson.sergio@picpay.com>
2022-06-11 16:33:34 -0300

    Add http handler

--- depinjection/greet.go
+++ depinjection/greet.go
@@ -3,8 +3,13 @@ package depinjection
 import (
 	"fmt"
 	"io"
+	"net/http"
 )
 
 func Greet(w io.Writer, name string) {
 	fmt.Fprintf(w, "Hello, %s", name)
+}
+
+func GreetHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
 }
--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"hello/depinjection"
+	"log"
+	"net/http"
 	"os"
 )
 
 func main() {
 	depinjection.Greet(os.Stdout, "Elodie")
+
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(depinjection.GreetHandler)))
 }
