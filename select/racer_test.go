dedf49090443b8d7744043ae27c9746d375fef84
Anderson L. Sergio <anderson.sergio@picpay.com>
2022-06-12 16:48:56 -0300

    Add timeout to website race

--- select/racer_test.go
+++ select/racer_test.go
@@ -1,6 +1,7 @@
-package _select
+package _select_test
 
 import (
+	_select "hello/select"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -8,20 +9,38 @@ import (
 )
 
 func TestRacer(t *testing.T) {
-	slowServer := newDelayedServer(20 * time.Millisecond)
-	defer slowServer.Close()
-	fastServer := newDelayedServer(0)
-	defer fastServer.Close()
+	t.Run("quickest response wins", func(t *testing.T) {
+		slowServer := newDelayedServer(20 * time.Millisecond)
+		defer slowServer.Close()
+		fastServer := newDelayedServer(0)
+		defer fastServer.Close()
 
-	slowURL := slowServer.URL
-	fastURL := fastServer.URL
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
 
-	want := fastURL
-	got := Racer(slowURL, fastURL)
+		want := fastURL
+		got, err := _select.Racer(slowURL, fastURL)
+		if err != nil {
+			t.Error("unexpected error but got :", err)
+		}
 
-	if want != got {
-		t.Errorf("want %q, got %q", want, got)
-	}
+		if want != got {
+			t.Errorf("want %q, got %q", want, got)
+		}
+	})
+
+	t.Run("must timeout if the response takes more than 10s", func(t *testing.T) {
+		serverA := newDelayedServer(11 * time.Second)
+		defer serverA.Close()
+		serverB := newDelayedServer(12 * time.Second)
+		defer serverB.Close()
+
+		_, err := _select.Racer(serverA.URL, serverB.URL)
+
+		if err == nil {
+			t.Error("error expected but didn't get any")
+		}
+	})
 }
 
 func newDelayedServer(delay time.Duration) *httptest.Server {
