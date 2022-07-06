2343066035d4ee7c199290fb0c879ef46712c5bc
Anderson L. Sergio <anderson.sergio@picpay.com>
2022-07-06 07:32:02 -0300

    Wire up filesystem store

--- webapp/api/server_integration_test.go
+++ webapp/api/server_integration_test.go
@@ -3,14 +3,20 @@ package api
 import (
 	"github.com/anlsergio/go-by-tests/webapp/model"
 	"github.com/anlsergio/go-by-tests/webapp/store"
+	"io"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 )
 
 func TestRecordingWinsAndRetrievingThem(t *testing.T) {
 	player := "Pepper"
-	server := NewPlayerServer(store.NewInMemoryPlayerStore())
+	db, cleanDB := createTempFile(t, "")
+	defer cleanDB()
+
+	s := &store.FileSystemPlayerStore{Database: db}
+	server := NewPlayerServer(s)
 
 	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
 	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
@@ -35,4 +41,22 @@ func TestRecordingWinsAndRetrievingThem(t *testing.T) {
 		got := getLeagueFromResponse(t, response.Body)
 		assertLeague(t, want, got)
 	})
+}
+
+func createTempFile(t testing.TB, initialData string) (fileBuffer io.ReadWriteSeeker, removeTempFile func()) {
+	t.Helper()
+
+	tempFile, err := os.CreateTemp("", "db")
+	if err != nil {
+		t.Fatalf("could not create temp file %v", err)
+	}
+
+	tempFile.Write([]byte(initialData))
+
+	removeTempFile = func() {
+		tempFile.Close()
+		os.Remove(tempFile.Name())
+	}
+
+	return tempFile, removeTempFile
 }
--- webapp/main.go
+++ webapp/main.go
@@ -5,10 +5,21 @@ import (
 	"github.com/anlsergio/go-by-tests/webapp/store"
 	"log"
 	"net/http"
+	"os"
 )
 
-func main() {
-	sv := api.NewPlayerServer(store.NewInMemoryPlayerStore())
+const dbFileName = "game.db.json"
 
-	log.Fatal(http.ListenAndServe(":8080", sv))
+func main() {
+	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatalf("could not open file %s, %v", dbFileName, err)
+	}
+
+	st := &store.FileSystemPlayerStore{Database: db}
+	sv := api.NewPlayerServer(st)
+
+	if err := http.ListenAndServe(":8080", sv); err != nil {
+		log.Fatalf("coult not listen on port 8080, %v", err)
+	}
 }
